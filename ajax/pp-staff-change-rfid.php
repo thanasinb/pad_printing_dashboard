41e08d0f9650d678e7ef5630254a5f16e5785847
phisic1714 <96192069+phisic1714@users.noreply.github.com>
2021-12-24 11:19:01 +0700

    Update pp-staff-change-rfid.php

--- ajax/pp-staff-change-rfid.php
+++ ajax/pp-staff-change-rfid.php
@@ -7,7 +7,8 @@ name_first='".$_GET['name_first']."',
 name_last='".$_GET['name_last']."',
 prefix='".$_GET['prefix']."',
 id_role='".$_GET['id_role']."',
-id_shif='".$_GET['id_shif']."'WHERE id_staff='" . $_GET['id_staff'] . "'";
+id_shif='".$_GET['id_shif']."',
+site='".$_GET['site']."'WHERE id_staff='" . $_GET['id_staff'] . "'";
 
 $result_staff = $conn->query($sql);
 
@@ -16,4 +17,3 @@ require '../update/terminate.php';
 echo json_encode(array("statusCode" => 200), JSON_UNESCAPED_UNICODE);
 
 ?>
-
