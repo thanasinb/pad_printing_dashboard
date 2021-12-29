f3998812f969eb16a910feedd1b9f8f22081b8d7
phisic1714 <96192069+phisic1714@users.noreply.github.com>
2021-12-29 14:08:59 +0700

    Update pp-staff-list-staff-script-technician.php

--- pp-staff-list-staff-script-technician.php
+++ pp-staff-list-staff-script-technician.php
@@ -1,26 +1,33 @@
 <?php
 ini_set('display_errors', 0);
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
 require 'update/establish.php';
-
-$_GET['id_role']=2;
-$sql = "SELECT * FROM staff WHERE id_role=" . $_GET['id_role'] . " ORDER BY id_role ASC, id_staff ASC";
+$sql = "SELECT * FROM staff WHERE id_role=	2  ORDER BY id_role ASC, id_staff ASC";
+$sql1 = "SELECT * FROM staff WHERE id_role= 8  ORDER BY id_role ASC, id_staff ASC";
 $result_staff = $conn->query($sql);
-
-while($data_staff = $result_staff->fetch_assoc()) {
+$result_staff1 = $conn->query($sql1);
+while($data_staff = $result_staff1->fetch_assoc()) {
     echo "<tr class='text-black fw-bold row_staff'>";
     echo "<td class='id_staff'>" . $data_staff['id_staff'] . "</td>";
-    echo "<td>" . $data_staff['id_rfid'] . "</td>";
-    echo "<td>" . $data_staff['name_first'] . " " . $data_staff['name_last'] . "</td>";
-    echo "<td>";
+    echo "<td class='rfid'>" . $data_staff['id_rfid'] . "</tdclass>";
+    echo "<td class='prefix'>";
+    $prefix = intval($data_staff['prefix']);
+    if ($prefix==1)
+        echo "นาย";
+    elseif ($prefix==2)
+        echo "นาง";
+    elseif ($prefix==3)
+        echo "นางสาว";
+    echo "<td>". $data_staff['name_first'] ." ". $data_staff['name_last']."</td>";
+
+    echo "<td class='role'>";
     $id_role = intval($data_staff['id_role']);
-    if ($id_role==1)
-        echo "Operator";
-    elseif ($id_role==2)
+    if ($id_role==2)
         echo "Technician";
+    elseif ($id_role==8)
+        echo "Senior Technician";
     echo "</td>";
-    echo "<td>" . $data_staff['id_shif'] . "</td>";
+    echo "<td class='shif' >" . $data_staff['id_shif'] . "</td>";
     echo "<td>" . "<div class='avatar avatar-xl me-3 bg-gray-200'><img class='avatar-img img-fluid' src='./images/staffs/" . $data_staff['staff_img'] . "'  alt=' ' /></div>" . "</td>";
     echo "<td>";
     echo "<button name='staff_edit' type='submit' class='btn btn-datatable btn-icon text-black me-2 staff_edit'>";
@@ -29,6 +36,39 @@ while($data_staff = $result_staff->fetch_assoc()) {
     echo "<i class='fas fa-trash'></i></button>";
     echo "</td>";
     echo "</tr>";
+
+}
+while($data_staff = $result_staff->fetch_assoc()) {
+    echo "<tr class='text-black fw-bold row_staff'>";
+    echo "<td class='id_staff'>" . $data_staff['id_staff'] . "</td>";
+    echo "<td class='rfid'>" . $data_staff['id_rfid'] . "</tdclass>";
+    echo "<td class='prefix'>";
+    $prefix = intval($data_staff['prefix']);
+    if ($prefix==1)
+        echo "นาย";
+    elseif ($prefix==2)
+        echo "นาง";
+    elseif ($prefix==3)
+        echo "นางสาว";
+    echo "<td>". $data_staff['name_first'] ." ". $data_staff['name_last']."</td>";
+
+    echo "<td class='role'>";
+    $id_role = intval($data_staff['id_role']);
+    if ($id_role==2)
+        echo "Technician";
+    elseif ($id_role==8)
+        echo "Senior Technician";
+    echo "</td>";
+    echo "<td class='shif' >" . $data_staff['id_shif'] . "</td>";
+    echo "<td>" . "<div class='avatar avatar-xl me-3 bg-gray-200'><img class='avatar-img img-fluid' src='./images/staffs/" . $data_staff['staff_img'] . "'  alt=' ' /></div>" . "</td>";
+    echo "<td>";
+    echo "<button name='staff_edit' type='submit' class='btn btn-datatable btn-icon text-black me-2 staff_edit'>";
+    echo "<i class='far fa-edit fs-6'></i></button>";
+    echo "<button name='staff_delete' type='submit' class='btn btn-datatable btn-icon text-black me-2 staff_delete'>";
+    echo "<i class='fas fa-trash'></i></button>";
+    echo "</td>";
+    echo "</tr>";
+
 }
 require 'update/terminate.php';
 ?>
