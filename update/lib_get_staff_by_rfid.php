5ec6f815233673be9546baf187ef21d65d4a1046
thanasinb <thanasin.b@gmail.com>
2022-08-29 21:01:08 +0700

    Fix cannot replace intval()

--- update/lib_get_staff_by_rfid.php
+++ update/lib_get_staff_by_rfid.php
@@ -4,6 +4,5 @@ function get_staff_by_rfid($conn, $id_rfid)
     $sql = "SELECT id_staff, name_first, name_last, id_role as role FROM staff WHERE id_rfid='" . $id_rfid . "'";
     $result = $conn->query($sql);
     $data = $result->fetch_assoc();
-    $data["role"] = intval($data["role"]);
     return $data;
 }
