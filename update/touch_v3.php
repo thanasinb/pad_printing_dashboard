aa0ddf510a8643e2993370f330062b2d8e85b9c0
thanasinb <thanasin.b@gmail.com>
2022-11-02 11:54:51 +0700

    check empty task

--- update/touch_v3.php
+++ update/touch_v3.php
@@ -13,27 +13,31 @@ if(empty($data_staff_rfid)){
 }
 else{
     $data_machine_queue = get_staff_from_machine_queue($conn, $_GET['id_mc']);
-    if($data_machine_queue['id_staff'] == null){
-        $data_planning = get_planning($conn, $data_machine_queue['id_task']);
-        $data_activity = get_qty_shif($conn, 'activity', $data_staff_rfid['id_staff'], $data_machine_queue['id_task']);
-        $data_json = json_encode(array_merge($data_staff_rfid, $data_planning, $data_activity), JSON_UNESCAPED_UNICODE);
+    if($data_machine_queue['id_task'] == null){
+        $data_json = json_encode(array('code'=>'003', 'message'=>'Machine has no task'), JSON_UNESCAPED_UNICODE);
     }else{
-        if($data_staff_rfid['role']==2){
-            $data_staff_queue = get_staff_by_id($conn, $data_machine_queue['id_staff']);
-            if($data_staff_queue['role']==1){
-                $data_activity = get_active_activity_by_id_and_machine($conn, 'activity_downtime', $data_staff_queue['id_staff'], $_GET['id_mc']);
-                if(!empty($data_activity)){
-                    $data_planning = get_planning($conn, $data_machine_queue['id_task']);
-                    $data_activity = get_qty_shif($conn, 'activity_downtime', $data_staff_rfid['id_staff'], $data_machine_queue['id_task']);
-                    $data_json = json_encode(array_merge($data_staff_rfid, $data_planning, $data_activity), JSON_UNESCAPED_UNICODE);
+        if($data_machine_queue['id_staff'] == null){
+            $data_planning = get_planning($conn, $data_machine_queue['id_task']);
+            $data_activity = get_qty_shif($conn, 'activity', $data_staff_rfid['id_staff'], $data_machine_queue['id_task']);
+            $data_json = json_encode(array_merge($data_staff_rfid, $data_planning, $data_activity), JSON_UNESCAPED_UNICODE);
+        }else{
+            if($data_staff_rfid['role']==2){
+                $data_staff_queue = get_staff_by_id($conn, $data_machine_queue['id_staff']);
+                if($data_staff_queue['role']==1){
+                    $data_activity = get_active_activity_by_id_and_machine($conn, 'activity_downtime', $data_staff_queue['id_staff'], $_GET['id_mc']);
+                    if(!empty($data_activity)){
+                        $data_planning = get_planning($conn, $data_machine_queue['id_task']);
+                        $data_activity = get_qty_shif($conn, 'activity_downtime', $data_staff_rfid['id_staff'], $data_machine_queue['id_task']);
+                        $data_json = json_encode(array_merge($data_staff_rfid, $data_planning, $data_activity), JSON_UNESCAPED_UNICODE);
+                    }else{
+                        $data_json = json_encode(array('code'=>'028', 'message'=>'Operator ID - DT table mismatch'), JSON_UNESCAPED_UNICODE);
+                    }
                 }else{
-                    $data_json = json_encode(array('code'=>'028', 'message'=>'Operator ID - DT table mismatch'), JSON_UNESCAPED_UNICODE);
+                    $data_json = json_encode(array('code'=>'027', 'message'=>'This machine is currently in occupied by tech ID: ' . $data_machine_queue['id_staff']), JSON_UNESCAPED_UNICODE);
                 }
             }else{
-                $data_json = json_encode(array('code'=>'027', 'message'=>'This machine is currently in occupied by tech ID: ' . $data_machine_queue['id_staff']), JSON_UNESCAPED_UNICODE);
+                $data_json = json_encode(array('code'=>'026', 'message'=>'This machine is currently in occupied by staff ID: ' . $data_machine_queue['id_staff']), JSON_UNESCAPED_UNICODE);
             }
-        }else{
-            $data_json = json_encode(array('code'=>'026', 'message'=>'This machine is currently in occupied by staff ID: ' . $data_machine_queue['id_staff']), JSON_UNESCAPED_UNICODE);
         }
     }
 }
