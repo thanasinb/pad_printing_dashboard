673172087c4c3f6771d5255435dbc2b1495391a5
thanasinb <thanasin.b@gmail.com>
2022-11-16 12:40:24 +0700

    fix role null

--- update/lib_get_staff_by_id.php
+++ update/lib_get_staff_by_id.php
@@ -2,10 +2,10 @@
 function get_staff_by_id($conn, $id_staff)
 {
     $sql = "SELECT id_rfid, id_staff, name_first, name_last, id_role AS role, id_shif AS team FROM staff WHERE id_staff='" . $id_staff . "'";
-    echo $sql;
+//    echo $sql;
     $result = $conn->query($sql);
     $data = $result->fetch_assoc();
-    echo "<br>" . $data['role'] . "<br>";
+//    echo "<br>" . $data['role'] . "<br>";
     $data['role']=intval($data['role']);
     return $data;
 }
