c5bea13a5c767720681d3d5e1ccfb1a22dfc0b68
thanasinb <thanasin.b@gmail.com>
2023-01-18 11:05:38 +0700

    Check activity activity in counting

--- update/lib_get_active_activity_by_id_and_machine.php
+++ update/lib_get_active_activity_by_id_and_machine.php
@@ -1,7 +1,7 @@
 <?php
-function get_active_activity_by_id_and_machine($conn, $table, $id_staff, $id_mc)
+function get_active_activity_by_id_and_machine($conn, $table, $status, $id_staff, $id_mc)
 {
-    $sql = "SELECT * FROM " . $table . " WHERE id_staff = " . $id_staff . " AND id_machine = '" . $id_mc . "' AND status_downtime=1";
+    $sql = "SELECT * FROM " . $table . " WHERE id_staff = " . $id_staff . " AND id_machine = '" . $id_mc . "' AND " . $status. "=1";
     $result = $conn->query($sql);
     $data = $result->fetch_assoc();
     return $data;
