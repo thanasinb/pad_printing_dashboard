42f8f2a0c3d1fbf11917bf373bd02b13cb3817b3
Thanasin <thanasin.b@gmail.com>
2023-01-23 16:17:58 +0700

    Log upload error

--- update/log.php
+++ update/log.php
@@ -7,22 +7,3 @@ $sql = "INSERT INTO log (log_datetime, log_description) VALUES ('" . $dt_text . 
 $conn->query($sql);
 echo $sql;
 require 'terminate.php';
-
-//require 'url.php';
-//$url = URL_EXTERNAL . 'update/log-bunnam.php?dt=' . $dt_text . '&log=' . $_GET['log'];
-//echo $url . "<br>";
-
-//$ch = curl_init();
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_URL, $url);
-//$result=curl_exec($ch);
-//curl_close($ch);
-//var_dump($result);
-
-//$result = file_get_contents($url);
-
-//var_dump(json_decode($result, true));
-//var_dump(json_decode($result, true));
-//header('Location: ' . $url . 'update/log-bunnam.php?dt=' . $dt_text . '&log=' . $_GET['log']);
-//die();
-
