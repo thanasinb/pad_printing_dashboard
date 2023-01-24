57c4471aab2f804deb207e31880540d67656f6da
thanasin <thanasin.b@gmail.com>
2023-01-24 15:09:46 +0700

    Add multiplier to DB

--- update/activity_v3.php
+++ update/activity_v3.php
@@ -7,6 +7,8 @@ require 'lib_add_activity_downtime.php';
 require 'lib_get_staff_from_machine_queue.php';
 require 'lib_get_active_activity_by_id_and_machine.php';
 require 'lib_add_log.php';
+require 'lib_get_planning.php';
+
 
 add_log($conn, basename($_SERVER['REQUEST_URI']));
 
@@ -16,12 +18,30 @@ if(empty($data_machine_queue['id_staff'])){
         $table = 'activity';
         $data_staff = get_staff_by_id($conn, $_GET['id_staff']);
         list($shif, $date_eff) = get_shif($conn, $_GET['id_staff'], $data_staff['team']);
-        $data_json = add_activity($conn, $table, $_GET['id_task'], $_GET['id_mc'], $_GET['id_staff'], $shif, $date_eff);
+        $data_planning = get_planning($conn, $_GET['id_task']);
+        $data_json = add_activity(
+            $conn,
+            $table,
+            $_GET['id_task'],
+            $_GET['id_mc'],
+            $_GET['id_staff'],
+            $shif,
+            $date_eff,
+            $data_planning['multiplier']);
     }elseif ($_GET['activity_type']==2){
         $table = 'activity_rework';
         $data_staff = get_staff_by_id($conn, $_GET['id_staff']);
         list($shif, $date_eff) = get_shif($conn, $_GET['id_staff'], $data_staff['team']);
-        $data_json = add_activity($conn, $table, $_GET['id_task'], $_GET['id_mc'], $_GET['id_staff'], $shif, $date_eff);
+        $data_planning = get_planning($conn, $_GET['id_task']);
+        $data_json = add_activity(
+            $conn,
+            $table,
+            $_GET['id_task'],
+            $_GET['id_mc'],
+            $_GET['id_staff'],
+            $shif,
+            $date_eff,
+            $data_planning['multiplier']);
     }elseif ($_GET['activity_type']==3){
         $table = 'activity_downtime';
         $data_machine_queue = get_staff_from_machine_queue($conn, $_GET['id_mc']);
@@ -47,7 +67,17 @@ if(empty($data_machine_queue['id_staff'])){
 //    }
         $data_staff = get_staff_by_id($conn, $_GET['id_staff']);
         list($shif, $date_eff) = get_shif($conn, $_GET['id_staff'], $data_staff['team']);
-        $data_json = add_activity_downtime($conn, $table, $_GET['id_task'], $_GET['id_mc'], $_GET['id_staff'], $shif, $date_eff, $_GET["code_downtime"]);
+        $data_planning = get_planning($conn, $_GET['id_task']);
+        $data_json = add_activity_downtime(
+            $conn,
+            $table,
+            $_GET['id_task'],
+            $_GET['id_mc'],
+            $_GET['id_staff'],
+            $shif,
+            $date_eff,
+            $_GET['code_downtime'],
+            $data_planning['multiplier']);
     }else{
         $data_json = json_encode(array('code'=>'005', 'message'=>'Invalid activity_type'), JSON_UNESCAPED_UNICODE);
     }
