6e5bc8924364432e3df8b7d122dde2a99c0726a3
thanasin <thanasin.b@gmail.com>
2023-01-26 21:54:30 +0700

    Check previous idle is empty?

--- update/lib_end_activity_idle.php
+++ update/lib_end_activity_idle.php
@@ -6,15 +6,17 @@ function end_activity_idle($conn, $id_machine)
     $result = $conn->query($sql);
     $data_activity = $result->fetch_assoc();
 
-    $time_current = strtotime($data_activity["time_current"]);
-    $time_start = strtotime($data_activity["time_start"]);
-    $time_duration_second = $time_current - $time_start;
-    $time_duration =  gmdate('H:i:s', $time_duration_second);
+    if (!empty($data_activity)){
+        $time_current = strtotime($data_activity["time_current"]);
+        $time_start = strtotime($data_activity["time_start"]);
+        $time_duration_second = $time_current - $time_start;
+        $time_duration =  gmdate('H:i:s', $time_duration_second);
 
-    $sql = "UPDATE activity_idle SET ";
-    $sql = $sql . "status_work=3,";
-    $sql = $sql . "time_end='" . $data_activity["time_current"] . "',";
-    $sql = $sql . "duration='" . $time_duration . "'";
-    $sql = $sql . " WHERE id_activity=" . $data_activity["id_activity"];
-    $result = $conn->query($sql);
+        $sql = "UPDATE activity_idle SET ";
+        $sql = $sql . "status_work=3,";
+        $sql = $sql . "time_end='" . $data_activity["time_current"] . "',";
+        $sql = $sql . "duration='" . $time_duration . "'";
+        $sql = $sql . " WHERE id_activity=" . $data_activity["id_activity"];
+        $result = $conn->query($sql);
+    }
 }
