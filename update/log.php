4a14907ca1b9042c32fe72287ccd7150d5a9dbe4
thanasinb <thanasin.b@gmail.com>
2023-06-21 10:17:02 +0700

    update log

--- update/log.php
+++ update/log.php
@@ -1,9 +1,14 @@
 <?php
+require 'lib_add_log.php';
 require 'establish.php';
-$dt = new DateTime('NOW');
-$dt_text = $dt->format('Y-m-d H:i:s');
+//$dt = new DateTime('NOW');
+//$dt_text = $dt->format('Y-m-d H:i:s');
 
-$sql = "INSERT INTO log (log_datetime, log_description) VALUES ('" . $dt_text . "', '". $_GET['log']. "')";
-$conn->query($sql);
+//$sql = "INSERT INTO log (log_datetime, log_description) VALUES ('" . $dt_text . "', '". $_GET['log']. "')";
+//$conn->query($sql);
 //echo $sql;
+
+add_log($conn, basename($_SERVER['REQUEST_URI']));
+add_log($conn, basename($_GET['log']));
+
 require 'terminate.php';
