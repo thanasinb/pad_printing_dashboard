72affa6e660e3a39d8211f21cbad9b162abbaf22
thanasinb <thanasin.b@gmail.com>
2023-09-27 11:26:03 +0700

    update time_previous when come back from break

--- update/lib_update_break.php
+++ update/lib_update_break.php
@@ -1,8 +1,8 @@
 <?php
 function update_break($conn, $table, $table_break, $str_activity, $str_status, $id_activity, $id_break, $time_current, $break_duration, $str_break, $total_break){
 
-    $sql = "UPDATE " . $table . " SET " . $str_status . "=1, " . $str_break . "='" .
-        $total_break . "' WHERE " . $str_activity . "=" . $id_activity;
+    $sql = "UPDATE " . $table . " SET " . $str_status . "=1, " . $str_break . "='" . $total_break . "', " .
+           "time_previous='" . $time_current . "' WHERE " . $str_activity . "=" . $id_activity;
     $result = $conn->query($sql);
 
     $sql = "UPDATE " . $table_break . " SET " .
