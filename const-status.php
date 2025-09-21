5c23825d44ea102934d74d6879d000fa51d6063e
thanasin <thanasin.b@gmail.com>
2025-09-21 13:20:11 +0700

    add activity and table constants

--- const-status.php
+++ const-status.php
@@ -5,4 +5,12 @@ const STATUS_CLOSED=3;
 const STATUS_EXPORTED=4;
 const STATUS_UPDATED=5;
 const STATUS_COMPLETE=6;
+
+const ACTIVITY_BACKFLUSH=1;
+const ACTIVITY_REWORK=2;
+const ACTIVITY_DOWNTIME=3;
+
+const TABLE_BACKFLUSH='activity';
+const TABLE_REWORK='activity_rework';
+const TABLE_DOWNTIME='activity_downtime';
 ?>
