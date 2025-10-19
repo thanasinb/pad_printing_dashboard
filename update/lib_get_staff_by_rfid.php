bce3037085a21f4875addc693ef549743e3326eb
thanasin <thanasin.b@gmail.com>
2025-10-19 18:27:28 +0700

    replace role by role group

--- update/lib_get_staff_by_rfid.php
+++ update/lib_get_staff_by_rfid.php
@@ -1,7 +1,14 @@
 <?php
 function get_staff_by_rfid($conn, $id_rfid)
 {
-    $sql = "SELECT id_staff, name_first, name_last, id_role as role FROM staff WHERE id_rfid='" . $id_rfid . "' AND active=1";
+    $sql = "SELECT 
+        staff.id_staff, 
+        staff.name_first, 
+        staff.name_last, 
+        role.role_group as role 
+        FROM staff 
+        LEFT JOIN role ON staff.id_role = role.id_role 
+        WHERE staff.id_rfid='" . $id_rfid . "' AND staff.active=1";
     $result = $conn->query($sql);
     $data = $result->fetch_assoc();
     return $data;
